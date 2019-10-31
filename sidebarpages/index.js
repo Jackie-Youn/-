39c6f54f6624d28a74ca849dc9e374c313ed7737
jackie <yjk807@naver.com>
2019-10-31 19:00:51 +0900

    basic web front

---
+++ sidebarpages/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url = require('url');
+var Web3 = require('web3');
+var web3 = new Web3('http://localhost:8545');
+
+var app = http.createServer(function(request,respose){
+    var _url = requres.url;
+    
+})
+
+app.listen(3000);
