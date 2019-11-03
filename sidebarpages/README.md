1c284acc0f73e6496702a8e8dfa8d2b2714e7b46
jackie <yjk807@naver.com>
2019-11-03 10:55:51 +0900

    set pages

--- sidebarpages/README.md
+++ sidebarpages/README.md
@@ -1,9 +1,12 @@
-# Frontend webpage
-## [Start Bootstrap - Simple Sidebar](https://startbootstrap.com/template-overviews/simple-sidebar/)
+# Save Promissory Note
+   This is a dApp connected to Ethereum blockchain that saves promissory note written and confirmed by two persons.
+   
+## Frontend webpage
+### [Start Bootstrap - Simple Sidebar](https://startbootstrap.com/template-overviews/simple-sidebar/)
 
 [Simple Sidebar](http://startbootstrap.com/template-overviews/simple-sidebar/) is an off canvas sidebar navigation template for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/).
 
-## Status
+### Status
 
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/BlackrockDigital/startbootstrap-simple-sidebar/master/LICENSE)
 [![npm version](https://img.shields.io/npm/v/startbootstrap-simple-sidebar.svg)](https://www.npmjs.com/package/startbootstrap-simple-sidebar)
@@ -11,8 +14,13 @@
 [![dependencies Status](https://david-dm.org/BlackrockDigital/startbootstrap-simple-sidebar/status.svg)](https://david-dm.org/BlackrockDigital/startbootstrap-simple-sidebar)
 [![devDependencies Status](https://david-dm.org/BlackrockDigital/startbootstrap-simple-sidebar/dev-status.svg)](https://david-dm.org/BlackrockDigital/startbootstrap-simple-sidebar?type=dev)
 
-## Copyright and License
+### Copyright and License
 
 Copyright 2013-2019 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-simple-sidebar/blob/gh-pages/LICENSE) license.
 
-#Backend Ethereum dApp
+
+## Backend Ethereum dApp
+
+### Save Promissory Note on Ethereum Blockchain
+
+ 
---
+++ sidebarpages/contract/note.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.5.1;
+
+contract Promissory{
+    
+    struct Promissory_note {
+        address creditor_addr;
+        address debtor_addr;
+        uint256 note_hash;
+        bool creditor_confirmed;
+        bool debtor_confirmed;
+    }
+    
+    mapping(uint => Promissory_note) public notes;
+    
+    event logNewNote(address creditor, address debtor);
+    event logConfirmed(address sender, uint256 _hash);
+    
+    
+    function create_note(address creditor, address debtor, uint256 _hash) public {
+        //require(msg.sender != address(0));
+        //require(debtor != address(0));
+        Promissory_note storage newNote = notes[_hash];
+        newNote.creditor_addr = creditor;
+        newNote.debtor_addr = debtor;
+        newNote.note_hash = _hash;
+        emit logNewNote(creditor, debtor);
+    }
+    
+    function confirm_note(address creditor,uint256 _hash) public{
+        if(notes[_hash].creditor_addr == msg.sender && notes[_hash].creditor_confirmed == false){
+            //require(notes[creditor].creditor_confirmed == false);
+            notes[_hash].creditor_confirmed = true;
+        }
+        else if(notes[_hash].debtor_addr == msg.sender && notes[_hash].creditor_confirmed == false){
+            //require(notes[creditor].creditor_confirmed == false);
+            notes[_hash].debtor_confirmed = true;
+        }
+        if(notes[_hash].creditor_confirmed && notes[_hash].debtor_confirmed){
+            emit logConfirmed(creditor, notes[_hash].note_hash);
+        }
+    }
+    
+    function check_note(uint256 _hash) public view returns (address number){
+        //require(creditor != address(0));
+        //require(debtor != address(0));
+        if(notes[_hash].creditor_addr == address(0)) number = address(0);
+        if(notes[_hash].debtor_addr == address(0)) number = address(0);
+        if(notes[_hash].creditor_confirmed && notes[_hash].debtor_confirmed) number = notes[_hash].creditor_addr;
+        else number = address(0);
+    }
+    
+}
